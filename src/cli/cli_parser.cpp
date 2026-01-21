aad87a2a8462fdedd05fdbbd4db22937e980d83a
Akash Arunkumar <mirrorghost007@gmail.com>
2026-01-21 16:02:09 +0000

    added init and insert function

--- src/cli/cli_parser.cpp
+++ src/cli/cli_parser.cpp
@@ -3,13 +3,21 @@
 
 int main(int argc ,char **argv) {
     CLI::App VeriStoreDB{"vsdb"} ;
-    auto* greet = VeriStoreDB.add_subcommand("greet", "Greet someone");
-    std::string name;
-    greet->add_option("name",name,"Nme to greet")->required();
-    CLI11_PARSE(VeriStoreDB, argc,argv)
-    if (VeriStoreDB.got_subcommand(greet)){
-        std::cout << "Hello" <<name<< "!\n";
-    }
     
+    
+    
+    //init command 
+    auto* init = VeriStoreDB.add_subcommand("init","initialize a new VSDB database ");
+
+    //table insertion command
+    auto* insert_cmd = VeriStoreDB.add_subcommand("insert", "insert into the table");
+    std::string insert_table ;
+    std::vector<std::string> insert_cols;
+    std::vector<std::string> insert_vals;
+    insert_cmd->add_option("table",insert_table,"Table name")->required();
+    insert_cmd->add_option("--coloumns,-c",insert_cols,"Coloumn names"  );
+    insert_cmd->add_option("--values,-v",insert_vals,"values");
+    CLI11_PARSE(VeriStoreDB, argc,argv)
+    if (VeriStoreDB.got_subcommand(init)) {std::cout << "initillizedaaa\n";};
     return 0;
 }
